46aea36f2712915a0c42c466a7b85ef80e432c16
AlmondRyan <ryan2551@foxmail.com>
2024-10-10 17:31:17 +0800

    Implement more of RetoMIString class. Add `c_str` and other functions to RetoMIString class.

--- Source/Engine3D/ModelImporter/Utility/RetoMIString/RetoMIString.cpp
+++ Source/Engine3D/ModelImporter/Utility/RetoMIString/RetoMIString.cpp
@@ -1,10 +1,19 @@
 #include "RetoMIString.h"
 
 namespace Retoccilus::Engine3D::ModelImporter {
-    RetoMIString::RetoMIString() noexcept : _length(0), data{'\0'} { }
+    /**
+     * @brief Default Constructor
+     */
+    RetoMIString::RetoMIString() noexcept : _length(0) {
+        data[0] = '\0';
+    }
 
+    /**
+     * @brief Copied Constructor that accept a ``RetoMIString`` instance.
+     * @param rms - The parameter that you need to copy
+     */
     RetoMIString::RetoMIString(const Retoccilus::Engine3D::ModelImporter::RetoMIString &rms)
-        : _length(rms._length), data{'\0'}
+            : _length(rms._length)
     {
         if (_length > MAX_SIZE - 1) {
             _length = MAX_SIZE - 1; // Crop the string to fit the buffer size.
@@ -15,13 +24,14 @@ namespace Retoccilus::Engine3D::ModelImporter {
 #else
         ::memcpy(data, rms.data, _length);
 #endif
-
-        // Add an ending tag `\0` to keep the string is a valid c-string.
-        data[_length] = '\0';
+        data[_length] = '\0'; // Add an ending tag `\0` to keep the string is a valid c-string.
     }
 
-    RetoMIString::RetoMIString(const std::string &str) : _length((uint32_t)str.length()), data{'\0'}
-    {
+    /**
+     * @brief Constructor that accept a ``std::string`` instance.
+     * @param str - The parameter that you need to copy
+     */
+    RetoMIString::RetoMIString(const std::string &str) : _length(static_cast<std::uint32_t>(str.length())) {
         if (_length > MAX_SIZE - 1) {
             _length = MAX_SIZE - 1; // Crop the string to fit the buffer size.
         }
@@ -31,7 +41,102 @@ namespace Retoccilus::Engine3D::ModelImporter {
 #else
         ::memcpy(data, str.c_str(), _length);
 #endif
-        // Add an ending tag `\0` to keep the string is a valid c-string.
-        data[_length] = '\0';
+        data[_length] = '\0'; // Add an ending tag `\0` to keep the string is a valid c-string.
+    }
+
+    void RetoMIString::setData(const std::string &str) {
+        _length = static_cast<std::uint32_t>(str.length());
+        if (_length > MAX_SIZE - 1) {
+            _length = MAX_SIZE - 1; // Crop the string to fit the buffer size.
+        }
+
+#ifdef _MSC_VER
+        ::memcpy_s(data, sizeof(data), str.c_str(), _length);
+#else
+        ::memcpy(data, str.c_str(), _length);
+#endif
+        data[_length] = '\0'; // Add an ending tag `\0` to keep the string is a valid c-string.
+    }
+
+    void RetoMIString::setData(const char *charPointerArray) {
+        auto lth = static_cast<std::uint32_t>(::strlen(charPointerArray));
+        if (lth > MAX_SIZE - 1) {
+            lth = MAX_SIZE - 1;
+        }
+
+        _length = lth;
+#ifdef _MSC_VER
+        ::memcpy_s(data, sizeof(data), charPointerArray, lth);
+#else
+        ::memcpy(data, charPointerArray, lth);
+#endif
+        data[lth] = '\0'; // Add an ending tag `\0` to keep the string is a valid c-string.
+    }
+
+    RetoMIString &RetoMIString::operator=(const Retoccilus::Engine3D::ModelImporter::RetoMIString &rms) {
+        if (this != &rms) {
+            _length = rms._length;
+            if (_length > MAX_SIZE - 1) {
+                _length = MAX_SIZE - 1;
+            }
+
+#ifdef _MSC_VER
+            ::memcpy_s(data, sizeof(data), rms.data, _length);
+#else
+            ::memcpy(data, rms.data, _length);
+#endif
+            data[_length] = '\0'; // Add an ending tag `\0` to keep the string is a valid c-string.
+        }
+        return *this;
+    }
+
+    RetoMIString &RetoMIString::operator=(const char *charPointerArray) {
+        setData(charPointerArray);
+        return *this;
+    }
+
+    RetoMIString &RetoMIString::operator=(const std::string &str) {
+        setData(str);
+        return *this;
+    }
+
+    bool RetoMIString::operator==(const Retoccilus::Engine3D::ModelImporter::RetoMIString &rms) const {
+        if (_length == rms._length) {
+            return ::memcmp(data, rms.data, _length) == 0;
+        }
+        return false;
+    }
+
+    bool RetoMIString::operator!=(const Retoccilus::Engine3D::ModelImporter::RetoMIString &rms) const {
+        return !(*this == rms);
+    }
+
+    void RetoMIString::append(const char *appender) {
+        const auto lth = static_cast<std::uint32_t>(::strlen(appender));
+        if (lth == 0) {
+            return;
+        }
+
+        if (lth + _length >= MAX_SIZE) {
+            return;
+        }
+
+#ifdef _MSC_VER
+        ::memcpy_s(data + _length, MAX_SIZE - _length, appender, lth);
+#else
+        ::memcpy(data + _length, appender, lth);
+#endif
+
+        _length += lth;
+        data[_length] = '\0'; // Add an ending tag `\0` to keep the string is a valid c-string.
+    }
+
+    void RetoMIString::clear() {
+        _length = 0;
+        data[0] = '\0';
+    }
+
+    const char *RetoMIString::c_str() const {
+        return data;
     }
 }
--- Source/Engine3D/ModelImporter/Utility/RetoMIString/RetoMIString.h
+++ Source/Engine3D/ModelImporter/Utility/RetoMIString/RetoMIString.h
@@ -12,10 +12,24 @@ namespace Retoccilus::Engine3D::ModelImporter {
         explicit RetoMIString() noexcept;
         RetoMIString(const RetoMIString &rms);
         explicit RetoMIString(const std::string &str);
+
+        void setData(const std::string &str);
+        void setData(const char *charPointerArray);
+
+        RetoMIString &operator=(const RetoMIString &rms);
+        RetoMIString &operator=(const char *charPointerArray);
+        RetoMIString &operator=(const std::string &str);
+
+        bool operator==(const RetoMIString &rms) const;
+        bool operator!=(const RetoMIString &rms) const;
+
+        void append(const char *appender);
+        void clear();
+        [[nodiscard]] const char* c_str() const;
     private:
-        unsigned int _length;
+        std::uint32_t _length;
         char data[MAX_SIZE]; // Max String Size
-    };
+    }; // End of Class: RetoMIString
 }
 
 #endif // RETO_MI_STRING_INCLUDE_H
